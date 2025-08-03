7532803c1c6d54d60f41ba1d4e97be189c17c9d9
sarabjitsinha <sinhasarabjit@gmail.com>
2025-08-03 19:21:53 +0530

    fixed bug

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -53,26 +53,6 @@ export const startChat = async (req, res) => {
 };
 
 // Approve/Reject request
-// export const respondChatRequest = async (req, res) => {
-//   const { groupId, accept } = req.body;
-
-//   const group = await Group.findById(groupId);
-
-//   if (!group) return res.status(404).json({ msg: "Group not found" });
-
-//   if (!group.members.includes(req.user._id)) return res.status(403).json({ msg: "Unauthorized" });
-
-//   if (accept) {
-//     group.approvedBy.push(req.user._id);
-//     await group.save();
-//     res.json({ msg: "Accepted" });
-//   } else {
-//     group.members = group.members.filter((id) => id.toString() !== req.user._id.toString());
-//     group.approvedBy = group.approvedBy.filter((id) => id.toString() !== req.user._id.toString());
-//     await group.save();
-//     res.json({ msg: "Rejected" });
-//   }
-// };
 
 export const respondChatRequest = async (req, res) => {
   const { groupId, accept } = req.body;
